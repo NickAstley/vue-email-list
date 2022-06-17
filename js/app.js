dffe94cf6a9d818a32d263ab40a0de716799a1a5
NickAstley <andreanicastro1994@gmail.com>
2022-06-17 17:25:27 +0200

    Added single emal request

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*
+Descrizione:
+Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+const appVue = new Vue({
+    el : "#app",
+    data : {
+        emailsList : []
+    },
+    methods : {
+        generateEmail() {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((axiosResponse) => {
+                    this.emailsList.push(axiosResponse.data.response);
+                    console.log(this.emailsList);
+                });
+        },
+        generateEmailsList() {
+            
+        }
+    }
+});
